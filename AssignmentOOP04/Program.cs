87a5372325d8cf2f2365ae0000ee5184cc39e40d
mohamedfarag22-dot <mohamedfarag2272001@gmail.com>
2026-03-08 21:01:02 +0200

    Qusetion01

--- AssignmentOOP04/Program.cs
+++ AssignmentOOP04/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿#region Part01:
+
+#region Question01:
+// Q1 : What is the difference between static binding and dynamic binding? When does each one happen?
+//
+//Asnwer: Static binding: means the method call is resolved at compile time. The compiler already knows which method will be executed.
+//      * When it happens:
+//        Method overloading.
+//        Static methods.
+//        Private methods.
+//        Final/Sealed methods.
+//        When a method is not overridden.
+//
+//       Dynamic binding: means the method call is resolved at runtime. The program decides which method to execute while running.
+//      * When it happens:
+//        Method overriding.
+//        Virtual methods.
+//        Abstract methods.
+//        Polymorphism.
+#endregion
+
+#endregion
